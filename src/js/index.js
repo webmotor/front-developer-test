a2cfaaa5cdbd721d67f6c802a8094baaac057991
Aleksandr <ascwork86@gmail.com>
2021-02-07 21:09:31 +0500

    add feedback

--- src/js/index.js
+++ src/js/index.js
@@ -17,9 +17,16 @@ new Swiper('.swiper-container', {
   //   touchRatio: 1,
   grabCursor: true,
   loop: true,
+  // mousewheel: {
+  //   sensitivity: 2,
+  // },
   effect: 'flip',
   flipEffect: {
     slideShadow: true,
     limitRotation: true,
   },
+});
+
+new Swiper('.swiper-container-feedback', {
+  effect: 0,
 });
